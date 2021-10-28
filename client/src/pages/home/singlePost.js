97bf461ae0563ba00419c5d531b7625224d08eb3
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2021-10-28 16:38:09 -0600

    single post render

--- client/src/pages/home/singlePost.js
+++ client/src/pages/home/singlePost.js
@@ -1,18 +1,22 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {useParams} from "react-router";
-import {PostsContext} from "../../context/postContext";
+// import {PostsContext} from "../../context/postContext";
 import BlogPostFinder from "../../apis/blogAPI";
 import "./singlePost.css";
 
 function SinglePost(props) {
   // hook to find the params in a url
   const {id} = useParams();
+  const [title, setTitle] = useState();
+  const [post, setPost] = useState();
 
-  const blogPost = useContext(PostsContext);
+  // const {posts, setPost} = useContext(PostsContext);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await BlogPostFinder.get(`/${id}`);
+      setTitle(response.data.blog_title);
+      setPost(response.data.post_content);
       console.log(response.data);
     };
 
@@ -21,29 +25,19 @@ function SinglePost(props) {
 
   return (
     <div className="single-post-container">
+      {/* {post.map((el) => {
+        return ( */}
       <div className="single-post">
         <img
           src="/images/snowy-mountains.jpg"
           alt="mountians"
           className="card-image"
         />
-        <h1 className="post-title">Blog title</h1>
-        <p className="post-info">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          lacinia eu odio et tincidunt. Fusce a augue dapibus, gravida est
-          vitae, fringilla magna. Integer blandit nisi sit amet tortor
-          fringilla, ac convallis libero condimentum. Fusce maximus luctus ex,
-          id tempor orci hendrerit et. Maecenas tristique condimentum volutpat.
-          Etiam non elit vel enim placerat consectetur. Proin eu urna sed massa
-          tristique volutpat non quis tellus. Donec feugiat rutrum tellus, non
-          blandit ex. Quisque sollicitudin, felis at facilisis tristique, mauris
-          ligula ultricies neque, id accumsan felis enim eget est. Vestibulum
-          ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-          curae; Nulla semper laoreet quam tempor rhoncus. Sed ornare enim
-          lacus, at interdum metus finibus in. Vestibulum ante ipsum primis in
-          faucibus orci luctus et ultrices posuere cubilia curae;
-        </p>
+        <h1 className="post-title">{title}</h1>
+        <p className="post-info">{post}</p>
       </div>
+      {/* );
+      })} */}
     </div>
   );
 }
